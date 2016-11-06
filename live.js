23ca65e451278249acf3365a87a4acddcd861319
Mitchell Katz <sk1er@lythrim.net>
2016-11-06 01:40:49 -0400

    Beta Release 1.0

--- live.js
+++ live.js
@@ -1,3 +1,68 @@
 /**
  * Created by Mitchell Katz on 11/6/2016.
  */
+var updateRate = 5 * 1000;
+
+var last = 0;
+function update() {
+    console.log("Dude its updating, you hyped. Maybe in 100k runs the veteran reward update will come out");
+    $.ajax({
+        dataType: "json",
+        url: "http://watchdog.cryptkpr.me:4567/current",
+        success: function(response) {
+
+                if(last === 0 ) {
+                    last = response.count;
+                }
+                var $start = $('#start'),
+                    start = last,
+                    $reset = $('#reset'),
+                    reset = $reset.get(0),
+                    $counter = $('#counter'),
+                    startVal = last,
+                    currentVal,
+                    endVal = response.total,
+                    prefix = 'Players: ',
+                    fontSize = $counter.css('font-size');
+                currentVal = startVal;
+                var i = setInterval(function ()
+                {
+                    if (currentVal === response.total) {
+                        clearInterval(i);
+
+                        $counter.text(prefix+currentVal);
+
+                    } else if(currentVal < response.total)
+                    {
+                        if(response-last<1000) {
+                            currentVal+=Math.abs(response-last)/1000;
+                        } else {
+                            currentVal++;
+                        }
+                        $counter.text(prefix+currentVal);
+                    } else {
+                        if(last-response>1000) {
+                            currentVal-=Math.abs(response-last)/1000;
+                        } else {
+                            currentVal--;
+                        }
+
+                        $counter.text(prefix+currentVal);
+                    }
+                }, 2000/Math.abs(last-currentVal));
+
+                last=endVal;
+
+            document.getElementById("number").innerHTML="Total bans: " + response.total + "<br>Last 60 seconds: " + response.perMinute;
+
+
+
+        }
+    });
+}
+
+$(document).ready(function() {
+    setInterval(update, updateRate);
+
+    update();
+});
