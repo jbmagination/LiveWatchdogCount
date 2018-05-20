48cfbee313b8a6161163040bd568bfef67d09288
jbmagination <26027089+jbmagination@users.noreply.github.com>
2018-05-20 13:05:01 -0400

    Update live.js
    trying to get it working

--- live.js
+++ live.js
@@ -40,7 +40,7 @@
                     }
                 }, 2000 / Math.abs(last - currentVal));
                 last = endVal;
-                document.getElementById("number").innerHTML = "Total bans: " + response.total + "<br>Last 60 seconds: " + response.perMinute;
+                document.getElementById("number").innerHTML = "Total bans: " + response.watchdog_total + "<br>Last 60 seconds: " + response.watchdog_lastMinute;
             }
         });
     }
