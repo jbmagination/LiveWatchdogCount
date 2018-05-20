39201c4042d5f86b504cdb92e354954cbd099ac0
jbmagination <26027089+jbmagination@users.noreply.github.com>
2018-05-20 13:22:12 -0400

    Update live.js
    try some more

--- live.js
+++ live.js
@@ -14,17 +14,16 @@
             dataType: "json",
             url: "https://api.plancke.io/watchdogStats",
             success: function(response) {
-		    console.log(response);
                 if (last === 0) {
-                    last = response.count;
+                    last = response.record.watchdog_total;
                 }
                 var $start = $('#start'), start = last, $reset = $('#reset'), reset = $reset.get(0), $counter = $('#counter'), startVal = last, currentVal, endVal = response.total, prefix = 'Players: ', fontSize = $counter.css('font-size');
                 currentVal = startVal;
                 var i = setInterval(function() {
-                    if (currentVal === response.total) {
+                    if (currentVal === response.record.watchdog_total) {
                         clearInterval(i);
                         $counter.text(prefix + currentVal);
-                    } else if (currentVal < response.total) {
+                    } else if (currentVal < response.record.watchdog_total) {
                         if (response - last < 1000) {
                             currentVal += Math.abs(response - last) / 1000;
                         } else {
@@ -41,7 +40,7 @@
                     }
                 }, 2000 / Math.abs(last - currentVal));
                 last = endVal;
-                document.getElementById("number").innerHTML = "Total bans: " + response.record.watchdog_total + "<br>Last 60 seconds: " + response.watchdog_lastMinute;
+                document.getElementById("number").innerHTML = "Total bans: " + response.record.watchdog_total + "<br>Last 60 seconds: " + response.record.watchdog_lastMinute;
             }
         });
     }
